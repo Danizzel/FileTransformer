c13e9190682c97bcb6c60fffdba99def0aa6a279
Danizzel <daniel.rodean@gmx.net>
2025-10-21 19:52:10 +0200

    Initial commit

---
+++ src/main/java/de/FileTransormer/FileTransformer/WordToPdf.java
@@ -0,0 +1,31 @@
+package de.FileTransormer.FileTransformer;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+
+public class WordToPdf {
+	
+	public static void wordToPdf(String fileLocation, String fileOutputLocation) {
+		
+		try {
+    		FileInputStream docFile = new FileInputStream(fileLocation);
+    		XWPFDocument doc = new XWPFDocument(docFile);
+    		PdfOptions pdfOptions = PdfOptions.create();
+    		FileOutputStream out = new FileOutputStream(fileOutputLocation);
+    		PdfConverter.getInstance().convert(doc, out, pdfOptions);
+    		doc.close();
+    		out.close();
+    		System.out.println("Done");
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    	}
+        
+	}
+
+}
